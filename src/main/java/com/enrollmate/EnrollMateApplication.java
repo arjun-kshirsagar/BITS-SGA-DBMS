ab8a5319a4ff97ba5296bbb3f3034ce2d769b934
arjun-kshirsagar <kshirsagararjun20@gmail.com>
2025-05-04 12:34:00 +0100

    Project Starter Code

---
+++ src/main/java/com/enrollmate/EnrollMateApplication.java
@@ -0,0 +1,13 @@
+package com.enrollmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enrollmate/EnrollMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enrollmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
